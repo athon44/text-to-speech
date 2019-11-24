255ae3900e457223f0b6c0d139f90a23aa632304
Junior <vilmarmendesjunior@hotmail.com>
2019-11-24 02:58:52 -0300

    hello world working

---
+++ index.js
@@ -0,0 +1,31 @@
+// Imports the Google Cloud client library
+const textToSpeech = require('@google-cloud/text-to-speech');
+
+// Import other required libraries
+const fs = require('fs');
+const util = require('util');
+async function main() {
+  // Creates a client
+  const client = new textToSpeech.TextToSpeechClient();
+
+  // The text to synthesize
+  const text = 'Olá, mundo!';
+
+  // Construct the request
+  const request = {
+    input: { text },
+    // Select the language and SSML Voice Gender (optional)
+    voice: { languageCode: 'pt-BR', ssmlGender: 'NEUTRAL' },
+    // Select the type of audio encoding
+    audioConfig: { audioEncoding: 'MP3' },
+  };
+
+  // Performs the Text-to-Speech request
+  const [response] = await client.synthesizeSpeech(request);
+  // Write the binary audio content to a local file
+  const writeFile = util.promisify(fs.writeFile);
+  await writeFile('output.mp3', response.audioContent, 'binary');
+  console.log('Audio content written to file: output.mp3');
+}
+
+main().catch(err => console.log(err));
